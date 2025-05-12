60e9a3ada1aa2b6507c33cce66d6970a965d0bca
ibdj <ibdjacobsen@gmail.com>
2025-05-12 10:11:57 +0100

    startet scrip to do analysis on subsections (5 plots aggregates)

---
+++ scripts/subsections_5plots_nmds.R
@@ -0,0 +1,3 @@
+#### nmds with subsections (5 plots) ####
+
+nero_subsection_summary <- readRDS("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/01 Vegetation changes Kobbefjord/data/nmds_nero/nmds_nero/data/nero_subsection_summary.rds")
