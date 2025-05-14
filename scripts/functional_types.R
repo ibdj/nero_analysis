16235c2f2b3535d6d2d1c6a9420a6cf7ee13522a
ibdj <ibdjacobsen@gmail.com>
2025-05-14 10:02:36 +0100

    importing the functional types for analysis

---
+++ scripts/functional_types.R
@@ -0,0 +1,5 @@
+#### subsections functional types ####
+
+
+func_type_subsection_summary <- readRDS("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/01 Vegetation changes Kobbefjord/data/nmds_nero/nmds_nero/data/func_type_subsection_summary.rds")
+
