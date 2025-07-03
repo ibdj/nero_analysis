e054bfeed464ac8c9a4129313f7552e4f36bc936
ibdj <ibdjacobsen@gmail.com>
2025-07-03 14:04:50 -0100

    left joined the function types from first import

--- scripts/importing_data.R
+++ scripts/importing_data.R
@@ -3,6 +3,7 @@
 # Load necessary libraries
 library(dplyr)
 library(readr)
+library(readxl)
 
 # Define the path to the subfolder containing your CSV files
 data_folder <- "data"
@@ -27,12 +28,17 @@ read_and_standardize <- function(file) {
   return(df)
 }
 
+# read the functional types files #
+eco_veg_growth_forms <- read_excel("data/eco_veg_growth_forms.xlsx")
+
 # Read and merge all CSV files into one data frame
 merged_data <- file_list |> 
   lapply(read_and_standardize) |>   # Apply standardization function to each file
   bind_rows() |>                    # Combine them into one data frame
   filter(raunkiaer_value != -9999) |>  # Remove invalid values
-  mutate(presence = ifelse(raunkiaer_value > 0, 1, 0))  # Convert to presence-absence
+  mutate(presence = ifelse(raunkiaer_value > 0, 1, 0)) |> # Convert to presence-absence
+  left_join(eco_veg_growth_forms, by = "taxon_code") |> 
+  select(!validation,)
 
 inspect <- merged_data |> 
   filter(year == 2022, 
@@ -41,6 +47,11 @@ inspect <- merged_data |>
 # View the merged data
 print(merged_data)
 merged_data$veg_type <- as.factor(merged_data$veg_type)
+merged_data$taxon_code <- as.factor(merged_data$taxon_code)
+merged_data$species <- as.factor(merged_data$species)
+merged_data$func_type <- as.factor(merged_data$func_type)
+merged_data$ecoveg_gfc <- as.factor(merged_data$ecoveg_gfc)
+merged_data$ecoveg_sgfc <- as.factor(merged_data$ecoveg_sgfc)
 summary(merged_data)
 
 write_rds(merged_data, "data/merged_data.rds")
