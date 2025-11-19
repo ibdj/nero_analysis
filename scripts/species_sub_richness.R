447918ad052ddf3f21507c7619e9c6c5c8185cf6
ibdj <ibdjacobsen@gmail.com>
2025-11-19 13:05:56 -0200

    deleted richness plots by simple regression

--- scripts/species_sub_richness.R
+++
@@ -1,1 +0,0 @@
-nero_subsection_pft <- readRDS("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/01 Vegetation changes Kobbefjord/data/nmds_nero/nmds_nero/data/nero_subsection_pft.rds")
