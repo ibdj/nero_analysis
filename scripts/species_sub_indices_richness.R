41cbc3539e684aeb985a582f3362532886499313
ibdj <ibdjacobsen@gmail.com>
2025-11-19 13:06:15 -0200

    renamed to include all indices

---
+++ scripts/species_sub_indices_richness.R
@@ -0,0 +1,1 @@
+nero_subsection_pft <- readRDS("~/Library/CloudStorage/OneDrive-Aarhusuniversitet/MappingPlants/01 Vegetation changes Kobbefjord/data/nmds_nero/nmds_nero/data/nero_subsection_pft.rds")
